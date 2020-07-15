6091f2e65621a8c8f5bf989dbcf2d0a1c763e8c7
Jeremy W <jtwillo51@gmail.com>
2020-07-14 18:43:57 -0600

    fail was the only really hard one

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,15 +5,39 @@ module.exports = {
   get,
 };
 
-function succeed(item) {
+function succeed(...specs) {
+  if (specs.length != 3) {
+    throw "Missing information!";
+  }
+  item = {
+    name: specs[0],
+    durability: specs[1],
+    enhancement: specs[2],
+  };
+
   return { ...item };
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 };
+  } else if (item.enhancement >= 15) {
+    return { ...item, durability: item.durability - 10 };
+  } else if (item.enhancement > 16) {
+    return {
+      ...item,
+      enhancement: item.enhancement - 1,
+      durability: item.durability - 10,
+    };
+  } else {
+    return { ...item };
+  }
 }
 
-function repair(item) {
+function repair({ item }) {
+  item = {
+    durability: 100,
+  };
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,32 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+const item = {
+  name: "iron sword",
+  durability: 25,
+  enhancement: 15,
+};
+
+test("repair", () => {
+  expect(enhancer.repair(item)).toEqual({ durability: 100 });
+  expect(enhancer.repair(item)).toEqual({ durability: 100 });
+});
+
+test("success", () => {
+  expect(enhancer.succeed("silver sword", 83, 15)).toEqual({
+    name: "silver sword",
+    durability: 83,
+    enhancement: 15,
+  });
+  // works as expected, comment in to show
+  //   expect(
+  //     enhancer.succeed("silver sword",  15 )
+  //   ).toThrow();
+});
+
+test("fail", () => {
+  expect(enhancer.fail(item)).toEqual({
+    name: "iron sword",
+    durability: 15,
+    enhancement: 15,
+  });
+});
